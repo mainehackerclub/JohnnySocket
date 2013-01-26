6848b3ac5378421056848bef4b32f23c76d61ad7
Garrett Wilkin <garrett.wilkin@gmail.com>
2013-01-26 12:09:00 -0500

    using stop function to stop the strobe

--- johnnyDemo.js
+++ johnnyDemo.js
@@ -9,7 +9,9 @@ var file = new(static.Server)('.');
 board.on("ready", function() {
     // Create an Led on pin 13 and strobe it on/off
     // Optionally set the speed; defaults to 100ms
-});
+  console.log('Board is ready');
+  led9 = new five.Led(9);
+  led13 = new five.Led(13);
 
 
 var serv = http.createServer(function (req, res) {
@@ -18,6 +20,10 @@ var serv = http.createServer(function (req, res) {
     // Serve files!
         file.serve(req, res);
     });
+    led13.stop();
+    led13.off();
+    led9.stop();
+    led9.off();
     
 }).listen(1337, '127.0.0.1');
 var io = require('socket.io').listen(serv);
@@ -25,32 +31,26 @@ io.sockets.on('connection', function(socket) {
   socket.on('ledOn',function(data) {
     console.log('ledOn',data.pin);
     console.log(data);
-    if (data.pin == 8) {
-      console.log('pin 8');
-      led8 = new five.Led(data.pin);
-      led8.strobe(data.strobe);
-    } else if (data.pin == 13) {
+    if (data.pin == 13) {
       console.log('pin 13');
-      led13 = new five.Led(data.pin);
       led13.strobe(data.strobe);
     } else if (data.pin == 9) {
       console.log('pin 9');
-      led9 = new five.Led(data.pin);
       led9.strobe(data.strobe);
     }
   });
   socket.on('ledOff',function(data) {
-    if (data.pin == 8) {
-      console.log('pin 8 off');
-      if (led8) { led8.off(); }
-    } else if (data.pin == 13) {
-      if (led13) { led13.off(); }
+    if (data.pin == 13) {
+      led13.stop();
+      led13.off();
       console.log('pin 13 off');
     } else if (data.pin == 9) {
-      if (led9) { led9.off(); }
+      led9.stop();
+      led9.off();
       console.log('pin 9 off');
     }
   });
 });
 
 console.log('Server running at http://127.0.0.1:1337/');
+});
