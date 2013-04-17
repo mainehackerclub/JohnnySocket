f2fc4629e76c2e5125e6cb48d7e81efc08be6c68
Garrett Wilkin <garrett.wilkin@gmail.com>
2013-04-17 00:01:29 -0400

    Pins better defined & changable in arduino.js.  TCP server, HTTP server, and socket.io all defined properly.  TCP connection no longer creates HTTP server.  That was embarrassing.  Proper writing & reading of data to sockets found in ClickSocket project.

--- arduino.js
+++ arduino.js
@@ -1,36 +1,67 @@
 var five = require("johnny-five"),
-    // or "./lib/johnny-five" when running from the source
     board, led3, led4, led5;
     board  = new five.Board();
+var util = require('util'),
+    net = require('net');
 
+// Global variables for active digital pins.
+var pins = [3, 4, 5],
+    leds = [];
+
+// Initialize array of Led objects.
+function setLeds() {
+  for (i in pins) {
+    var pin = pins[i];
+    var led = new five.Led(pin);
+    led.name = pin;
+    leds.push(led);
+    console.log('created Led '+pin);
+  };
+}
+
+// Find Led given a pin
+function getLed(name) {
+  var found = '';
+  for (i in leds) {
+    if (leds[i].name == name) {
+      found = leds[i];
+    }
+  }
+  return found;
+}
+
+// Define arduino behavior.
 board.on("ready", function() {
-  // Create an Led on pin 5 and strobe it on/off
-  // Optionally set the speed; defaults to 100ms
   console.log('Board is ready');
-  led3 = new five.Led(4);
-  led4 = new five.Led(4);
-  led5 = new five.Led(5);
+  setLeds();
 
-  var net = require('net');
+  // Make TCP connection as client.
   var client = net.connect(
-    {host:'66.172.10.186',port:'8124'},
+    {host:'localhost',port:'8124'},
     function() {//'connect' listener
-      console.log('client connected');
-      //client.write('world!\r\n');
-    });
+      console.log('TCP client connected');
+    }
+  );
+
+  // Process data from TCP Server.
   client.on('data', function(data) {
-    console.log(data.toString());
-    if (data == 'led3') {
-      console.log('Match');
-      led3.strobe(100);
-    } else {
-      console.log('Match FAIL');
+    var data = JSON.parse(data.toString()),
+      led = getLed(data.pin);
+    console.log('Received TCP data: '+data);
+    if (data.action === 'strobe') {
+      console.log('strobing '+led.name+' on period '+data.period+'ms.');
+      led.strobe(data.period);
+    } else if (data.action === 'off') {
+       led.stop().off();
     }
    // client.end();
   });
+
+  // Handle TCP end.
   client.on('end',function() {
-    console.log('client disconnected');
+    console.log('TCP client disconnected');
   });
-  led4.strobe(100);
+
 });
-console.log('Server running at http://127.0.0.1:1337/');
+
+console.log('Waiting for Arduino to be ready.');
--- server.js
+++ server.js
@@ -1,40 +1,48 @@
 var http = require('http');
-var dnode = require('dnode');
 var static = require('node-static');
 var file = new(static.Server)('.');
+var net = require('net');
+var tcpClients = [];
 
+// Handle TCP client connections.
+var server = net.createServer(function(c) { //'connection' listener
+
+  console.log('TCP client connected');
+  c.on('end', function() {
+    console.log('TCP client disconnected');
+    tcpClients.pop();
+  });
+
+  c.pipe(c);
+  tcpClients.push(c);
+});
+server.listen(8124, function() {
+  console.log('TCP server bound');
+});
+
+// Create HTTP server
 var serv = http.createServer(function (req, res) {
-    console.log('A Connection!');
+    console.log('HTTP Connection');
     req.addListener('end', function () {
     // Serve files!
         file.serve(req, res);
     });
 }).listen(1337, '127.0.0.1');
 
+// Initialize Socket.io
 var io = require('socket.io').listen(serv);
+io.set('log level',1);
+
+//Pass socket.io events to TCP streams.
 io.sockets.on('connection', function(socket) {
   socket.on('ledOn',function(data) {
-    console.log('ledOn',data.pin);
     console.log(data);
+    tcpClients[0].write(JSON.stringify(data));
   });
   socket.on('ledOff',function(data) {
-    console.log('ledOff',data.pin);
+    tcpClients[0].write(JSON.stringify(data));
     console.log(data);
   });
 });
-
-var net = require('net');
-var server = net.createServer(function(c) { //'connection' listener
-  console.log('server connected');
-  c.on('end', function() {
-    console.log('server disconnected');
-  });
-  c.write('led3');
-  c.pipe(c);
-});
-server.listen(8124, function() { //'listening' listener
-  console.log('server bound');
-});
-
 
 console.log('Server running at http://127.0.0.1:1337/');
